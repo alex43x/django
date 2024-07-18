2b8f64c4bcae413b848d721ae1668677425341ee
pancrasio43 <alexgllamosas@gmail.com>
2024-07-18 00:22:17 -0400

    First commit

--- loginTask/settings.py
+++ loginTask/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'login_task',
-        'USER': 'sample_user',
-        'PASSWORD': 'password',
+        'USER': 'pancrasio43',
+        'PASSWORD': 'Sapotoro?1',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -128,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/tasks'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
